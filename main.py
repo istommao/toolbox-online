0935a22ff2185a58339a153d8e4e9e503d7ea851
codingcat <istommao@gmail.com>
2021-04-28 23:40:48 +0800

    Modify qrcode page style

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.include_router(common.router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', reload=True, host='127.0.0.1', port=7340)
